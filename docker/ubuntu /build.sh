6a8a233eb264e4b2c527bbb7b9640e4cc4a5cc61
yurhett <46419702+yurhett@users.noreply.github.com>
2023-10-23 05:12:27 -0500

    Create build.sh

---
+++ docker/ubuntu /build.sh
@@ -0,0 +1,13 @@
+docker build \
+    --build-arg BASE_IMAGE=${BASE_IMAGE} \
+    --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
+    --build-arg PYTORCH_VERSION=${PYTORCH_VERSION} \
+    --build-arg PYTORCH_VERSION_SUFFIX=${PYTORCH_VERSION_SUFFIX} \
+    --build-arg TORCHVISION_VERSION=${TORCHVISION_VERSION} \
+    --build-arg TORCHVISION_VERSION_SUFFIX=${TORCHVISION_VERSION_SUFFIX} \
+    --build-arg TORCHAUDIO_VERSION=${TORCHAUDIO_VERSION} \
+    --build-arg TORCHAUDIO_VERSION_SUFFIX=${TORCHAUDIO_VERSION_SUFFIX} \
+    --build-arg PYTORCH_DOWNLOAD_URL=${PYTORCH_DOWNLOAD_URL} \
+    -t cnstark/pytorch:${IMAGE_TAG} \
+    -f docker/ubuntu/Dockerfile \
+    .
